e19a8ecc9a92c92cf1d78631b9a2190ed0681055
Rund Abu <rundabu@gmail.com>
2026-04-06 16:45:40 -0400

    Update MainWebsite.java

--- WebsiteComponents/MainWebsite.java
+++ WebsiteComponents/MainWebsite.java
@@ -6,27 +6,39 @@ public class MainWebsite extends JFrame implements ActionListener {
     
     public static void main(String[] args)
     {
+        
         // Creating instance of loginFrame
         JFrame loginFrame = new JFrame();
-            Label l1 = new Label("Enter valid Empid and Password");
+            JLabel l1 = new JLabel("Enter valid Empid and Password");
             l1.setBounds(100, 50, 120, 80);
             loginFrame.add(l1);
             JButton logonButton = new JButton("Login");
-            text password = new text();
-            text username = new text();
-            logonButton.addActionListener(password, username);
+            JTextField password = new JTextField();
+            JTextField username = new JTextField();
+            logonButton.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    //Logic handler
+                }
+            });
             logonButton.setBounds(150, 200, 220, 50);
-            loginFrame.add(logonButton);
-            loginFrame.setSize(500, 600);
-            loginFrame.setLayout(null);
+            
+
+            // Panel for layouts
+            JPanel p1 = new JPanel();
+            p1.add(password);
+            p1.add(username);
+            p1.add(logonButton);
+
+            loginFrame.add(p1);
+
+
+            loginFrame.setSize(700, 800);
+            p1.setBounds(0,0,700,800);
+            p1.setLayout(null);
             loginFrame.setVisible(true);
 
     
         
     }
 
-    public void actionPerformed(ActionEvent e)
-    {
-        
-    }
 }
