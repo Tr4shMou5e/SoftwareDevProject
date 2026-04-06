2b393c0fa595f7d92cfe7d5972da4f2db03181d6
Rund Abu <rundabu@gmail.com>
2026-04-06 15:55:43 -0400

    Update MainWebsite.java

--- WebsiteComponents/MainWebsite.java
+++ WebsiteComponents/MainWebsite.java
@@ -1,6 +1,32 @@
-import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class MainWebsite {
+public class MainWebsite extends JFrame implements ActionListener {
+    
+    public static void main(String[] args)
+    {
+        // Creating instance of loginFrame
+        JFrame loginFrame = new JFrame();
+            Label l1 = new Label("Enter valid Empid and Password");
+            l1.setBounds(100, 50, 120, 80);
+            loginFrame.add(l1);
+            JButton logonButton = new JButton("Login");
+            text password = new text();
+            text username = new text();
+            logonButton.addActionListener(password, username);
+            logonButton.setBounds(150, 200, 220, 50);
+            loginFrame.add(logonButton);
+            loginFrame.setSize(500, 600);
+            loginFrame.setLayout(null);
+            loginFrame.setVisible(true);
 
+    
+        
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        
+    }
 }
